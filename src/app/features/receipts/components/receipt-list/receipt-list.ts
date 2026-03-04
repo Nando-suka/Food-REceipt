7f7f94b62a49c2f82cd3564da180d911fb8944e0
Nando-Suka <jorginando5@gmail.com>
2026-03-04 15:25:18 +0700

    feat: implement receipt list component

--- src/app/features/receipts/components/receipt-list/receipt-list.ts
+++ src/app/features/receipts/components/receipt-list/receipt-list.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ReceiptService } from '../../../../core/services/receipt.service';
+import { Receipt } from '../../models/receipt.model';
 @Component({
   selector: 'app-receipt-list',
   imports: [],
   templateUrl: './receipt-list.html',
   styleUrl: './receipt-list.css',
 })
-export class ReceiptList {
 
+export class ReceiptList implements OnInit{
+    receipts : Receipt[] = [];
+
+    constructor(private receiptService: ReceiptService) {}
+
+    ngOnInit(): void {
+      this.loadReceipts();
+    }
+
+    loadReceipts(): void {
+      this.receipts = this.receiptService.getReceipts();
+    }
+
+    deleteReceipt(id: string): void {
+      this.receiptService.deleteReceipt(id);
+      this.loadReceipts();
+    }
 }
