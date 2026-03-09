067d7cc607ef061d2a0e02197d505702caee20a7
Nando-Suka <jorginando5@gmail.com>
2026-03-09 13:24:37 +0700

    feat: fixing error in receipt-stats.ts

--- src/app/core/services/receipt.ts
+++ src/app/core/services/receipt.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Receipt } from './../../features/receipts/models/receipt.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Receipt {
-  
+export class ReceiptService {
+
+  private storageKey = 'food_receipts';
+
+  getReceipts(): Receipt[] {
+    const data = localStorage.getItem(this.storageKey);
+    return data ? JSON.parse(data) : [];
+  }
+
+  addReceipt(receipt: Receipt): void {
+    const receipts = this.getReceipts();
+    receipts.push(receipt);
+    localStorage.setItem(this.storageKey, JSON.stringify(receipts));
+  }
+
+  deleteReceipt(id: string): void {
+    const receipts = this.getReceipts().filter(r => r.id !== id);
+    localStorage.setItem(this.storageKey, JSON.stringify(receipts));
+  }
 }
--- src/app/features/receipts/components/receipt-stats/receipt-stats.ts
+++ src/app/features/receipts/components/receipt-stats/receipt-stats.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReceiptService } from '../../../../core/services/receipt.service';
+import { Receipt } from '../../models/receipt.model';
 @Component({
   selector: 'app-receipt-stats',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './receipt-stats.html',
   styleUrl: './receipt-stats.css',
 })
-export class ReceiptStats {
+
+export class ReceiptStats implements OnInit{
+
+  // Make a initialitation stats
+  totalReceipts = 0
+  totalSpending = 0
+  averageSpending = 0
+  mostVisitedRestaurant = '';
+  totalItems = 0;
+
+  constructor(private receiptService: ReceiptService) {}
+
+  ngOnInit(): void {
+    this.calculateStats();
+  }
+
+  calculateStats(): void {
+
+    const receipts: Receipt[] = this.receiptService.getReceipts();
+
+    this.totalReceipts = receipts.length;
+
+    this.totalSpending = receipts.reduce(
+      (sum, r) => sum + r.totalAmount,
+      0
+    );
+
+    this.averageSpending = this.totalReceipts
+      ? this.totalSpending / this.totalReceipts
+      : 0;
+
+    this.totalItems = receipts.reduce(
+      (sum, r) => sum + r.items.reduce(
+        (itemSum, item) => itemSum + item.quantity,
+        0
+      ),
+      0
+    );
+
+    const restaurantCount: { [key: string]: number } = {};
+
+    receipts.forEach(r => {
+      restaurantCount[r.restaurantName] =
+        (restaurantCount[r.restaurantName] || 0) + 1;
+    });
+
+    this.mostVisitedRestaurant =
+      Object.keys(restaurantCount).reduce(
+        (a, b) =>
+          restaurantCount[a] > restaurantCount[b] ? a : b,
+        Object.keys(restaurantCount)[0] || ''
+      );
+
+  }
 
 }
