6305f8b39492e2cc33c53716e5747916c63f1718
Nando-Suka <jorginando5@gmail.com>
2026-03-09 13:28:38 +0700

    feat: integrate receipt statistics dsahboard into main app

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReceiptFormComponent } from './features/receipts/components/receipt-form/receipt-form';
 import { ReceiptListComponent } from './features/receipts/components/receipt-list/receipt-list';
+import { ReceiptStats } from './features/receipts/components/receipt-stats/receipt-stats';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     ReceiptFormComponent,
-    ReceiptListComponent
+    ReceiptStats,
+    ReceiptListComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
